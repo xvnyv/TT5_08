e3997e38e44274130ae2d7eca3dde807af6a636b
Jovial <103516970+jovialgithub@users.noreply.github.com>
2022-05-06 16:47:18 +0800

    Add files via upload

--- Sidebar.js
+++ Sidebar.js
@@ -1,14 +1,43 @@
 import React from "react";
-import Project from "./Project";
-import Form from "./Form"
 
 function Sidebar() {
+    
+    const [project, setProject] = React.useState([])
 
-    return (
-        <div className="sidebar">
-            <Form />
-        </div>
-    )
+    useEffect(() => {
+
+        // declare the data fetching function
+        
+        const fetchData = async () => {
+        
+        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoxLCJleHAiOjE2NTE4MzAyMTZ9.l3B1mtfcsVH9VmaCP_RSeSOSNr7HfOaq9cbBLKBgz3o";
+        
+        const config = {headers: {Authorization: `Bearer ${token}`}}
+        
+        const data = await axios.get("http://localhost:5000/projects", config);
+        
+        // TODO update project state here with response
+        
+        };
+        
+        
+        
+        // call the function
+        
+        fetchData()
+        
+        // make sure to catch any error
+        
+        .catch(console.error);
+        
+        }, []);
+
+
+    
+
+
+
+    return ()
 }
 
-export default Sidebar;
+export default Sidebar
